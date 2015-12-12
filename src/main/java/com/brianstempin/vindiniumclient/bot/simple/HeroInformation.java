cb1ad851a68ea2392ca31beb1b26bbd575a38569
kiara <kiarawestbrooks@gmail.com>
2015-12-12 04:22:03 -0800

    HeroInformation creation

---
+++ src/main/java/com/brianstempin/vindiniumclient/bot/simple/HeroInformation.java
@@ -0,0 +1,11 @@
+package com.brianstempin.vindiniumclient.bot.simple;
+
+import java.util.List;
+
+import com.brianstempin.vindiniumclient.dto.GameState.Position;
+
+public class HeroInformation {
+
+	public static Integer heroMineAmount;
+	public static List<Position> heroMineLocations;
+}
